2595cc1567f99a021507dad6e74229efc04053df
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2024-09-20 13:55:10 +0000

    fixed follower count issue

--- src/component/artists.ts
+++ src/component/artists.ts
@@ -102,7 +102,7 @@ export async function GenerateArtistsLinks(artistsArray: Array<string>) {
             console.log('Follower Counts undefined  :: ', followers )
             continue;
           } else if (typeof followers === "string") {
-            const numFolloweris = eval(followers.split(' ')[0])
+            const numFolloweris = eval(followers.split(' ')[0].split(',').join(''))
             console.log(numFolloweris)
             if(numFolloweris < 40000){
               console.log('follower count is lower than 100k')
